b78a944b399d3308e46be25a37291238d33ff06f
Rémi Campistron <remi.campistron@gmail.com>
2025-10-28 11:53:52 +0100

    Ajout de commentaires

--- src/BaseTP1Game.java
+++ src/BaseTP1Game.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 public class BaseTP1Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// ouvre un scanner pour enregister les entrées utilisateur
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Voulez vous jouer à un jeu ? (Oui ou Non) : ");
+		// récupère l'entrée utilisateur
 		String play = scan.next();
 		
+		// l'utilisateur commence le jeu
 		if(play.equals("o") || play.equals("Oui")  || play.equals("oui")) {
+			// le nombre d'essais pour trouver le bon nombre
 			int triesNb = 0;
+			// le nombre à trouver
 			double targetNumber = Math.floor(Math.random() * 100)  ;
 	
 			System.out.println("Saisir un nombre entre 1 et 100");
 			triesNb ++;
 			
-			
+			// on boucle sur l'entrée utilisateur, à chaque essai on incrémente le nombre d'essais
 			while (scan.hasNextInt()) {
 				System.out.println("Saisir un nombre entre 1 et 100");
 				int numberChoice  = scan.nextInt();
 				triesNb ++;
-				
+				System.out.println("le numero à trouver " + targetNumber);
 				if(numberChoice < targetNumber) {
 					System.out.println("Le nombre donné est inférieur au nombre à trouver");
 					triesNb ++;
@@ -28,7 +32,18 @@ public class BaseTP1Game {
 					System.out.println("Le nombre  donné est supérieur au nombre à trouver");
 					triesNb ++;
 				} else if(numberChoice == targetNumber) {
+					// l'utilisateur a deviné le nombre s'il le souhaite il peut continuer la partie
 					System.out.println("Bravo, vous avez trouvé le nombre en "+ triesNb + " coups" );
+					System.out.println("Voulez-vous continuer ?  (Oui ou Non) :");
+					play = scan.next();
+					if(play.equals("o") || play.equals("Oui")  || play.equals("oui")) {
+						triesNb = 0;
+						System.out.println("Saisir un nombre entre 1 et 100");
+						targetNumber = Math.floor(Math.random() * 100)  ;
+						continue;						
+					} else {
+						System.exit(0);
+					}
 				}
 			}
 			
