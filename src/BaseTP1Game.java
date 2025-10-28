0caee3a7bcd1c8ab3f25577021d9335b262eaee2
Rémi Campistron <remi.campistron@gmail.com>
2025-10-28 09:43:24 +0100

    Ajout de l'arrondi à l'entier pour le nombre à deviner

--- src/BaseTP1Game.java
+++ src/BaseTP1Game.java
@@ -7,18 +7,19 @@ public class BaseTP1Game {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Voulez vous jouer à un jeu ? (Oui ou Non) : ");
 		String play = scan.next();
-		System.out.println("Play : " + play);
+		
 		if(play.equals("o") || play.equals("Oui")  || play.equals("oui")) {
-			double targetNumber =  Math.random() * 100;
-			System.out.println("Le nombre est" + targetNumber);
-			System.out.println("Saisir un nombre entre 1 et 100");
-			
-			
-			
 			int triesNb = 0;
+			double targetNumber = Math.floor(Math.random() * 100)  ;
+	
+			System.out.println("Saisir un nombre entre 1 et 100");
+			triesNb ++;
+			
+			
 			while (scan.hasNextInt()) {
+				System.out.println("Saisir un nombre entre 1 et 100");
 				int numberChoice  = scan.nextInt();
-				
+				triesNb ++;
 				
 				if(numberChoice < targetNumber) {
 					System.out.println("Le nombre donné est inférieur au nombre à trouver");
@@ -27,7 +28,7 @@ public class BaseTP1Game {
 					System.out.println("Le nombre  donné est supérieur au nombre à trouver");
 					triesNb ++;
 				} else if(numberChoice == targetNumber) {
-					System.out.println("Bravo, vous avez trouvé le nombre en "+ triesNb + "coups" );
+					System.out.println("Bravo, vous avez trouvé le nombre en "+ triesNb + " coups" );
 				}
 			}
 			
